fe3bdb1b86b66b80e59b5d65026ba9383ecad7ab
rain9155 <jianyu9155@gmail.com>
2019-07-25 21:23:38 +0800

    updata

--- app/src/main/java/com/example/hy/svgtest/MainActivity.java
+++ app/src/main/java/com/example/hy/svgtest/MainActivity.java
@@ -8,6 +8,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.example.hy.svgtest.R;
+
 public class MainActivity extends AppCompatActivity {
 
     ImageView ivPath, ivSystem, ivSearch;
@@ -16,30 +18,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        ivPath = findViewById(R.id.iv_path);
-//        ivSystem = findViewById(R.id.iv_system);
-//        ivSearch = findViewById(R.id.iv_search);
-//        Drawable drawable = ivSystem.getDrawable();
-//        if(drawable instanceof Animatable){
-//            ((Animatable)drawable).start();
-//        }
-//        ivPath = findViewById(R.id.iv_path);
-//        ivPath.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Drawable drawable = ivPath.getDrawable();
-//                if(drawable instanceof Animatable){
-//                    ((Animatable)drawable).start();
-//                }
-//            }
-//        });
-//        ivSearch.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                if(ivSearch.getDrawable() instanceof Animatable){
-//                    ((Animatable)ivSearch.getDrawable()).start();
-//                }
-//            }
-//        });
+        ivPath = findViewById(R.id.iv_path);
+        ivSystem = findViewById(R.id.iv_system);
+        ivSearch = findViewById(R.id.iv_search);
+        Drawable drawable = ivSystem.getDrawable();
+        if(drawable instanceof Animatable){
+            ((Animatable)drawable).start();
+        }
+        ivPath = findViewById(R.id.iv_path);
+        ivPath.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Drawable drawable = ivPath.getDrawable();
+                if(drawable instanceof Animatable){
+                    ((Animatable)drawable).start();
+                }
+            }
+        });
+        ivSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ivSearch.getDrawable() instanceof Animatable){
+                    ((Animatable)ivSearch.getDrawable()).start();
+                }
+            }
+        });
     }
 }
